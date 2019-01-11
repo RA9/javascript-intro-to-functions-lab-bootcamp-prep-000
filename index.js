090b32ba5dfd716ed692c03014bc9a95e411feaf
Carlos Nah <cnah27@gmail.com>
2019-01-11 16:10:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ function shout(string) {
 function sayHiToGrandma(string) {
   if(string.toLowerCase()){
     return "I can't hear you!"
-  } else {
-    return string.toLowerCase()
+  } else if (string.toUpperCase()) {
+    return "YES INDEED!"
+  } else if (string === "I love you, Grandma.") {
+    return "I love you, too."
   }
    
 }
